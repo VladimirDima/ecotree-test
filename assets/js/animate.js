4dea94aeee7f9999ba19f112518bb1827432cf89
Vladimir Dima <dimavladimir14@gmail.com>
2021-11-03 13:04:23 +0100

    added line animation in viewport and replaced img tags with inline svg for less http requests

---
+++ assets/js/animate.js
@@ -0,0 +1,30 @@
+(() => {
+    let ec = window.ec || {};
+
+    ec.animate = {
+        init() {
+            const elm = document.querySelectorAll('.line');
+            if (elm) {
+                requestAnimationFrame(() => {
+                    this.addClassInView(elm);
+                });
+            }
+        },
+
+        addClassInView(elm) {
+            elm.forEach((item) => {
+                const scrollPos = window.pageYOffset;
+                const elmTop = item.getBoundingClientRect().top;
+                if (scrollPos - elmTop > 0) {
+                    item.classList.add("line-draw");
+                }
+
+            });
+            requestAnimationFrame(() => {
+                this.addClassInView(elm);
+            });
+        }
+    };
+
+    ec.animate.init();
+})();
